8cb809d9d3f5a04c05fed063049c928e08290af2
Alexandros Doukas <doukasfensing@gmail.com>
2021-07-31 11:23:32 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   port: process.env.PORT || 8080,
   db: {
-    prod: 'mongodb+srv://alexasimakis:<alexasimakis>@mvc.8hij0.mongodb.net/mvc?retryWrites=true&w=majority',
+    prod: process.env.DATABASE_URL,,
     test: process.env.DATABASE_URL,
     options: {
       useNewUrlParser: true,
