f1996324cebbabd46e68f9fabb873749d6e8b169
Alexandros Doukas <doukasfensing@gmail.com>
2021-07-31 11:24:23 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   port: process.env.PORT || 8080,
   db: {
-    prod: process.env.DATABASE_URL,,
+    prod: process.env.DATABASE_URL,
     test: process.env.DATABASE_URL,
     options: {
       useNewUrlParser: true,
